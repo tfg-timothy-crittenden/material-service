81d244315d0eaba218e96f849118020226a4e67a
unknown <tim_critt@hotmail.com>
2026-04-03 12:23:09 +0200

    Initial Db setup complete

--- reset-flyway.sql
+++
@@ -1,3 +0,0 @@
--- Reset Flyway history
-DROP TABLE IF EXISTS flyway_schema_history CASCADE;
-
--- src/main/resources/db/migration/R__seed_exam_family.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO exam_family (id, code, name, description)
-VALUES
-  (1, 'TOEFL', 'TOEFL', 'Test of English as a Foreign Language'),
-  (2, 'CAE', 'CAE', 'Cambridge English: Advanced'),
-  (3, 'FCE', 'FCE', 'Cambridge English: First')
-ON CONFLICT (id) DO NOTHING;
-
